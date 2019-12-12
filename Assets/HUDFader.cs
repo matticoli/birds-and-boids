0e6377263ddafc1418d5294a972147a4c95dcfeb
Mikel Matticoli <mikelmm99@gmail.com>
2019-12-12 19:29:06 +0000

    Fade-out HUD

---
+++ Assets/HUDFader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDFader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float f = gameObject.GetComponent<CanvasRenderer>().GetAlpha();
+        gameObject.GetComponent<CanvasRenderer>().SetAlpha(f - 0.025f);
+
+    }
+}
