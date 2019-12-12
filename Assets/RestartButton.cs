fe1276ea243992431c0570ace22873a00c21fa4d
Boids Boids <boids@mailinator.com>
2019-12-12 19:36:13 +0000

    Added title and restart button

---
+++ Assets/RestartButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RestartButton : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(RestartGame);
+        GameObject.Find("Button").GetComponentInChildren<Text>().text = "Restart";
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+    }
+
+}
---
+++ Assets/Title.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Title : MonoBehaviour
+{
+    const float period = 4.0f;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > period)
+        {
+            Destroy(gameObject);
+        }
+
+        Color colorOfObject = GetComponent<Text>().color;//Changed this
+        float prop = (Time.time / period);
+        colorOfObject.a = Mathf.Lerp(1, 0, prop);
+        GetComponent<Text>().color = colorOfObject;//Changed this
+    }
+
+}
